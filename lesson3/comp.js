fb6d222e818a0e7b6c389d2f55fa028ddea4d7e9
Maks <kpasowsky@gmail.com>
2023-08-28 01:51:41 +0300

    lesson 3 Vue

--- lesson3/comp.js
+++ lesson3/comp.js
@@ -5,7 +5,6 @@ Vue.component('blog-details', {
             <img src="Photo.png" alt="photo">
             <p>Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable.
             Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.</p>
-            
         </div>
     `,
 });
@@ -101,9 +100,6 @@ Vue.component('blogs-list', {
 Vue.component('blog', {
     props: ['blog'],    
     template: `
-            
-            
                 <li>{{ blog.id }} {{ blog.article }}</li>
-            
     `,
 });
